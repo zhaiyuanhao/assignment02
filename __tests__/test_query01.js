51bb08115c2da160b80812564a1eeafcd082aef9
jyl-jyl <97498760+jyl-jyl@users.noreply.github.com>
2023-03-10 01:14:50 -0500

    need to determine timeout for each test

--- __tests__/test_query01.js
+++ __tests__/test_query01.js
@@ -1,7 +1,7 @@
 import { expect, describe, it, jest } from '@jest/globals';
 import './jest_extensions';
 
-// jest.setTimeout(500000);
+jest.setTimeout(500000);
 
 describe('Query 01', () => {
   it('should return the correct results', async () => {
