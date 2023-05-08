53962e20868c203da00fdba926dceb488c904386
Mjumbe Poe <mjumbewu@gmail.com>
2023-05-08 11:42:12 -0400

    Updates to the load_data script

--- __scripts__/load_data.sh
+++ __scripts__/load_data.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+POSTGRES_NAME=${POSTGRES_NAME:-musa_509}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+POSTGRES_USER=${POSTGRES_USER:-postgres}
+POSTGRES_PASS=${POSTGRES_PASS:-postgres}
+PYTHON_COMMAND=${PYTHON_COMMAND:-python}
+
 SCRIPTDIR=$(readlink -f $(dirname $0))
 DATADIR=$(readlink -f $(dirname $0)/../__data__)
 mkdir -p ${DATADIR}
@@ -18,57 +24,62 @@ mkdir -p ${DATADIR}
 # Download and unzip gtfs data
 curl -L https://github.com/septadev/GTFS/releases/download/v202302261/gtfs_public.zip > ${DATADIR}/gtfs_public.zip
 unzip -o ${DATADIR}/gtfs_public.zip -d ${DATADIR}/gtfs_public
-unzip -o ${DATADIR}/gtfs_public/google_bus.zip -d google_bus
-unzip -o ${DATADIR}/gtfs_public/google_rail.zip -d google_rail
+unzip -o ${DATADIR}/gtfs_public/google_bus.zip -d ${DATADIR}/google_bus
+unzip -o ${DATADIR}/gtfs_public/google_rail.zip -d ${DATADIR}/google_rail
 
 # Create database and initialize table structure
 PGPASSWORD=postgres createdb \
   -h localhost \
-  -p 5432 \
+  -p ${POSTGRES_PORT} \
   -U postgres \
-  musa_509
+  ${POSTGRES_NAME}
 PGPASSWORD=postgres psql \
   -h localhost \
-  -p 5432 \
+  -p ${POSTGRES_PORT} \
   -U postgres \
-  -d musa_509 \
+  -d ${POSTGRES_NAME} \
   -f "${SCRIPTDIR}/create_tables.sql"
 
 # Load trip gtfs data into database
+sed -i 's/\r//g' ${DATADIR}/google_bus/stops.txt
 PGPASSWORD=postgres psql \
   -h localhost \
-  -p 5432 \
+  -p ${POSTGRES_PORT} \
   -U postgres \
-  -d musa_509 \
+  -d ${POSTGRES_NAME} \
   -c "\copy septa.bus_stops FROM '${DATADIR}/google_bus/stops.txt' DELIMITER ',' CSV HEADER;"
 
+# Use sed to replace \r\n with \n in the google_bus/routes.txt file
+sed -i 's/\r//g' ${DATADIR}/google_bus/routes.txt
 PGPASSWORD=postgres psql \
   -h localhost \
-  -p 5432 \
+  -p ${POSTGRES_PORT} \
   -U postgres \
-  -d musa_509 \
+  -d ${POSTGRES_NAME} \
   -c "\copy septa.bus_routes FROM '${DATADIR}/google_bus/routes.txt' DELIMITER ',' CSV HEADER;"
 
+sed -i 's/\r//g' ${DATADIR}/google_bus/trips.txt
 PGPASSWORD=postgres psql \
   -h localhost \
-  -p 5432 \
+  -p ${POSTGRES_PORT} \
   -U postgres \
-  -d musa_509 \
+  -d ${POSTGRES_NAME} \
   -c "\copy septa.bus_trips FROM '${DATADIR}/google_bus/trips.txt' DELIMITER ',' CSV HEADER;"
 
-
+sed -i 's/\r//g' ${DATADIR}/google_bus/shapes.txt
 PGPASSWORD=postgres psql \
   -h localhost \
-  -p 5432 \
+  -p ${POSTGRES_PORT} \
   -U postgres \
-  -d musa_509 \
+  -d ${POSTGRES_NAME} \
   -c "\copy septa.bus_shapes FROM '${DATADIR}/google_bus/shapes.txt' DELIMITER ',' CSV HEADER;"
 
+sed -i 's/\r//g' ${DATADIR}/google_rail/stops.txt
 PGPASSWORD=postgres psql \
   -h localhost \
-  -p 5432 \
+  -p ${POSTGRES_PORT} \
   -U postgres \
-  -d musa_509 \
+  -d ${POSTGRES_NAME} \
   -c "\copy septa.rail_stops FROM '${DATADIR}/google_rail/stops.txt' DELIMITER ',' CSV HEADER;"
 
 
@@ -77,36 +88,39 @@ curl -L 'https://api.census.gov/data/2020/dec/pl?get=NAME,GEO_ID,P1_001N&for=blo
 # unzip -o ${DATADIR}/census_population.zip -d ${DATADIR}/census_population
 
 
-python -c <<EOF
+${PYTHON_COMMAND} <<EOF
 import csv
 import json
 import pathlib
-​
+
 RAW_DATA_DIR = pathlib.Path('${DATADIR}')
 PROCESSED_DATA_DIR = pathlib.Path('${DATADIR}')
-​
+
 with open(
     RAW_DATA_DIR / 'census_population_2020.json',
     'r', encoding='utf-8',
 ) as infile:
     data = json.load(infile)
-​
+
 with open(
     PROCESSED_DATA_DIR / 'census_population_2020.csv',
     'w', encoding='utf-8',
 ) as outfile:
     writer = csv.writer(outfile)
-    writer.writerows(data)
+    writer.writerows([
+        (row[1], row[0], row[2])
+        for row in data
+    ])
 EOF
 
 
 # load data into database
 PGPASSWORD=postgres psql \
   -h localhost \
-  -p 5432 \
+  -p ${POSTGRES_PORT} \
   -U postgres \
-  -d musa_509 \
-  -c "\copy census.population_2020. FROM '${DATADIR}/census_population_2020.csv' DELIMITER ',' CSV HEADER;"
+  -d ${POSTGRES_NAME} \
+  -c "\copy census.population_2020 FROM '${DATADIR}/census_population_2020.csv' DELIMITER ',' CSV HEADER;"
 
 
 # Download and unzip pwd parcel data
@@ -116,7 +130,7 @@ unzip -o ${DATADIR}/phl_pwd_parcels.zip -d ${DATADIR}/phl_pwd_parcels
 # load parcel data into database
 ogr2ogr \
     -f "PostgreSQL" \
-    PG:"host=localhost port=$PGPORT dbname=$PGNAME user=$PGUSER password=$PGPASS" \
+    PG:"host=localhost port=${POSTGRES_PORT} dbname=${POSTGRES_NAME} user=${POSTGRES_USER} password=${POSTGRES_PASS}" \
     -nln phl.pwd_parcels \
     -nlt MULTIPOLYGON \
     -t_srs EPSG:4326 \
@@ -127,11 +141,11 @@ ogr2ogr \
 
 
 # Download philly neighborhood data
-curl -L https://github.com/azavea/geo-data/blob/9e0ac39840803d6218f4503e8a16c7aad0807de4/Neighborhoods_Philadelphia/Neighborhoods_Philadelphia.geojson > ${DATADIR}/Neighborhoods_Philadelphia.geojson
+curl -L https://github.com/azavea/geo-data/raw/9e0ac39840803d6218f4503e8a16c7aad0807de4/Neighborhoods_Philadelphia/Neighborhoods_Philadelphia.geojson > ${DATADIR}/Neighborhoods_Philadelphia.geojson
 # load neighbourhoods data into database
 ogr2ogr \
     -f "PostgreSQL" \
-    PG:"host=localhost port=$PGPORT dbname=$PGNAME user=$PGUSER password=$PGPASS" \
+    PG:"host=localhost port=${POSTGRES_PORT} dbname=${POSTGRES_NAME} user=${POSTGRES_USER} password=${POSTGRES_PASS}" \
     -nln azavea.neighborhoods \
     -nlt MULTIPOLYGON \
     -lco GEOMETRY_NAME=geog \
@@ -140,13 +154,12 @@ ogr2ogr \
     "${DATADIR}/Neighborhoods_Philadelphia.geojson"
 
 # Download and unzip census data
-curl -L https://www2.census.gov/geo/tiger/TIGER2020/BG/tl_2020_42_bg.zip
- > ${DATADIR}/census_blockgroups_2020.zip
+curl -L https://www2.census.gov/geo/tiger/TIGER2020/BG/tl_2020_42_bg.zip > ${DATADIR}/census_blockgroups_2020.zip
 unzip -o ${DATADIR}/census_blockgroups_2020.zip -d ${DATADIR}/census_blockgroups_2020
 # Load census data into database
 ogr2ogr \
     -f "PostgreSQL" \
-    PG:"host=localhost port=$PGPORT dbname=$PGNAME user=$PGUSER password=$PGPASS" \
+    PG:"host=localhost port=${POSTGRES_PORT} dbname=${POSTGRES_NAME} user=${POSTGRES_USER} password=${POSTGRES_PASS}" \
     -nln census.blockgroups_2020 \
     -nlt MULTIPOLYGON \
     -lco GEOMETRY_NAME=geog \
